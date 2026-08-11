3eab13135c4370d715b1a9a54af61f5fded6b677
Sagarsingh84 <sagarsingh83305@gmail.com>
2026-08-12 00:48:32 +0530

    Add binary search, bubble sort, and updated linear search

---
+++ Binary_Search.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int key)
+{
+    int low = 0, high = n - 1, mid;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key)
+        {
+            return mid;
+        }
+        else if(key < arr[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int n, arr[100], key, i, result;
+
+    printf("Enter length of array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements in sorted order:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    result = binarySearch(arr, n, key);
+
+    if(result != -1)
+    {
+        printf("Element found at index %d", result);
+    }
+    else
+    {
+        printf("Element not found");
+    }
+
+    return 0;
+}
