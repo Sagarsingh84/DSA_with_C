c878079786c6cb34eec78c2ee53ab8fe663220c6
Sagarsingh84 <sagarsingh83305@gmail.com>
2026-08-12 00:44:27 +0530

    linear search

---
+++ linear_practice.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, i, key;
+
+    printf("Enter length of array :");
+    scanf("%d",&n);
+
+    printf("Enter %d elements :")
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d",arr[i]);
+    }
+
+    
+
+
+
+}
