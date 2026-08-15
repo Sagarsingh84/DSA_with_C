78c0e3080c86b21a6deb045c3424c6ad2ba8c665
Sagar Singh <sagarsingh83305@gmail.com>
2026-08-15 11:30:03 +0530

    Add basic calculator functionality in Calculator.c
    Implement a basic calculator with addition, subtraction, multiplication, and division functionalities.

---
+++ C_Program_Assignment/Calculator.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a,b;
+    int choice;
+    printf("\n1.Additon\n2.Substraction\n3.Multiplication\n4.Division\n");
+    printf("Enter choice :");
+    scanf("%d",&choice);
+    switch(choice){
+        case 1:
+            printf("Enter 1st value :");
+            scanf("%d",&a);
+            printf("Enter 2nd value :");
+            scanf("%d",&b);
+            printf("Addition: %d",a+b);
+        break;
+        case 2:
+            printf("Enter 1st value :");
+            scanf("%d",&a);
+            printf("Enter 2nd value :");
+            scanf("%d",&b);
+            printf("Subtraction: %d",a-b);
+        break;
+        case 3:
+            printf("Enter 1st value :");
+            scanf("%d",&a);
+            printf("Enter 2nd value :");
+            scanf("%d",&b);
+            printf("Multiplication: %d",a*b);
+        break;
+        case 4:
+            printf("Enter 1st value :");
+            scanf("%d",&a);
+            printf("Enter 2nd value :");
+            scanf("%d",&b);
+            printf("Division: %d",a/b);
+        break;
+        
+        default:
+            printf("Invalid Choice");
+    }
+    return 0;
+}
